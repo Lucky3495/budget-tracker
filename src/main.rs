18a7b766e610e34af7a89da631c13f90476ba1b2
Lucky3495 <sab3495@gmail.com>
2025-04-28 15:00:58 +0300

    Added `clap` crate and used it for arguement parsing

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,46 @@
+#![allow(unused)]
 use budget_tracker::{Args, Config, log};
 use std::{env, process};
+use clap::{crate_name, crate_version, value_parser, Arg, Command};
 
 fn main() {
-    let input_data = Args::build(env::args()).unwrap_or_else(|err| {
-        eprintln!("Cannot parse arguments: {err}");
-        process::exit(1);
-    });
 
-    println!("{:?}", input_data);
+    let matches = Command::new(crate_name!())
+        .version(crate_version!())
+        .about("Logs expenses in a CSV file for personal analysis.")
+        .arg(
+            Arg::new("money")
+            .value_name("MONEY")
+            .value_parser(value_parser!(f64))
+            .help("The value of the expense as a number. Will be truncated to 2 decimal places.")
+            .required(true)
+        )
+        // TODO: Make the possible categories a config for the app, validate
+        // them, and display them in the --help output.
+        // For now the user is free to sepcify any one they want
+        .arg(
+            Arg::new("category")
+            .value_name("CATEGORY")
+            .help("The category of the expense.")
+            .required(true)
+        )
+        .arg(
+            Arg::new("comment")
+            .value_name("COMMENT")
+            .help("Comment that describes the expense.")
+        ).get_matches();
 
-    let config = Config {
-        file_path: "test.csv".to_string()
-    };
+        let args = Args {
+            money: *matches.get_one("money").expect("Money is required"),
+            category: matches.get_one::<String>("category").expect("Category is required").clone(),
+            comment: match matches.get_one::<String>("comment") {
+                Some(v) => Some(v.clone()),
+                None => None,
+            }
+        };
 
-    log(input_data, &config).unwrap();
+        let config = Config { file_path: "test.csv".to_string(), };
+
+        log(args, &config).unwrap()
 
 }
